be278e5d8c8396ef539b761a8a302e13ef0a1559
rtsunoda <ryantsunoda@gmail.com>
2020-10-29 19:58:28 +0000

    linux tweak

--- bash_profile/linux.bash
+++ bash_profile/linux.bash
@@ -2,37 +2,37 @@ alias linux.install.tools="install_linux_tools"
 
 # very similar to dockerfiles/devenv
 install_linux_tools() {
-  apt-get update && \
-  apt-get install -y coreutils && \
-  apt-get install -y iputils-ping && \
-  apt-get install -y wget && \
-  apt-get install -y curl && \
-  apt-get install -y nmap && \
-  apt-get install -y ngrep && \
-  apt-get install -y tmux && \
-  apt-get install -y vim && \
-  apt-get install -y tree && \
-  apt-get install -y git && \
-  apt-get install -y shunit2 && \
-  apt-get install -y grip && \
-  apt-get install -y jq && \
-  apt-get install -y highlight && \
-  apt-get install -y pandoc && \
-  apt-get install -y silversearcher-ag && \
-  apt-get install -y ripgrep && \
-  apt-get install -y openjdk-8-jdk && \
-  apt-get install -y openjdk-11-jdk && \
-  apt-get install -y openssh-server && \
-  apt-get install -y universal-ctags && \
-  apt-get install -y postgresql && \
-  service ssh start 
+  sudo apt-get update && \
+  sudo apt-get install -y coreutils && \
+  sudo apt-get install -y iputils-ping && \
+  sudo apt-get install -y wget && \
+  sudo apt-get install -y curl && \
+  sudo apt-get install -y nmap && \
+  sudo apt-get install -y ngrep && \
+  sudo apt-get install -y tmux && \
+  sudo apt-get install -y vim && \
+  sudo apt-get install -y tree && \
+  sudo apt-get install -y git && \
+  sudo apt-get install -y shunit2 && \
+  sudo apt-get install -y grip && \
+  sudo apt-get install -y jq && \
+  sudo apt-get install -y highlight && \
+  sudo apt-get install -y pandoc && \
+  sudo apt-get install -y silversearcher-ag && \
+  sudo apt-get install -y ripgrep && \
+  sudo apt-get install -y openjdk-8-jdk && \
+  sudo apt-get install -y openjdk-11-jdk && \
+  sudo apt-get install -y openssh-server && \
+  sudo apt-get install -y universal-ctags && \
+  sudo apt-get install -y postgresql && \
+  sudo service ssh start 
 
   # docker
-  apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-  apt-get update
-  apt-get install -y docker-ce docker-ce-cli containerd.io
+  # apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
+  # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  # add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  # apt-get update
+  # apt-get install -y docker-ce docker-ce-cli containerd.io
 
   # fzf
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -40,6 +40,7 @@ install_linux_tools() {
 
   # nvm
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+  nvm install --lts
 
   # go?
   # clojure/lein?
