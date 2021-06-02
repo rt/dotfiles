a3086033394923d6094c728e7cd7a46a6ad61b51
rtsunoda <ryantsunoda@gmail.com>
2021-06-02 05:55:28 -1000

    add playground setup

--- profile/commands/fd.bash
+++ profile/commands/fd.bash
@@ -1,2 +1,3 @@
-alias fd.dir.git="fd $(git rev-parse --show-toplevel) -t d --exclude node_modules"
+
+fd.dir.git="fd $(git rev-parse --show-toplevel) -t d --exclude node_modules"
 
---
+++ profile/setup/playground.bash
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+alias setup.playground="setup_playground"
+
+setup_playground() {
+
+  sess=playground
+
+  #----- js
+  tmux new-session -s $sess -d -n js
+  tmux send-keys -t $sess:js "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:js "cd ~/projects/dotfiles/quick-reference/javascript" Enter
+  tmux send-keys -t $sess:js "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:js
+  tmux send-keys -t $sess:js.2 "cd ~/projects/dotfiles/quick-reference/javascript" Enter
+
+  tmux select-pane -t $sess:js.1
+
+  #----- java
+  tmux new-window -t $sess -n java
+  tmux send-keys -t $sess:java "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:java "cd ~/projects/dotfiles/quick-reference/java" Enter
+  tmux send-keys -t $sess:java "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:java
+  tmux send-keys -t $sess:java.2 "cd ~/projects/dotfiles/quick-reference/java" Enter
+
+  tmux select-pane -t $sess:java.1
+
+  #----- postgres
+  tmux new-window -t $sess -n postgres
+  tmux send-keys -t $sess:postgres "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:postgres "cd ~/projects/dotfiles/quick-reference/postgres" Enter
+  tmux send-keys -t $sess:postgres "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:postgres
+  tmux send-keys -t $sess:postgres.2 "cd ~/projects/dotfiles/quick-reference/postgres" Enter
+
+  tmux select-pane -t $sess:postgres.1
+
+  #----- bash
+  tmux new-window -t $sess -n bash
+  tmux send-keys -t $sess:bash "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:bash "cd ~/projects/dotfiles/quick-reference/bash" Enter
+  tmux send-keys -t $sess:bash "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:bash
+  tmux send-keys -t $sess:bash.2 "cd ~/projects/dotfiles/quick-reference/bash" Enter
+
+  tmux select-pane -t $sess:bash.1
+
+  #----- python
+  tmux new-window -t $sess -n python
+  tmux send-keys -t $sess:python "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:python "cd ~/projects/dotfiles/quick-reference/python" Enter
+  tmux send-keys -t $sess:python "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:python
+  tmux send-keys -t $sess:python.2 "cd ~/projects/dotfiles/quick-reference/python" Enter
+
+  tmux select-pane -t $sess:python.1
+
+  #----- go
+  tmux new-window -t $sess -n go
+  tmux send-keys -t $sess:go "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:go "cd ~/projects/dotfiles/quick-reference/go" Enter
+  tmux send-keys -t $sess:go "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:go
+  tmux send-keys -t $sess:go.2 "cd ~/projects/dotfiles/quick-reference/go" Enter
+
+  tmux select-pane -t $sess:go.1
+
+  #----- clojure
+  tmux new-window -t $sess -n clojure
+  tmux send-keys -t $sess:clojure "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:clojure "cd ~/projects/dotfiles/quick-reference/clojure" Enter
+  tmux send-keys -t $sess:clojure "vim repl.md" Enter
+  
+  tmux split-window -v -l 24 -t $sess:clojure
+  tmux send-keys -t $sess:clojure.2 "cd ~/projects/dotfiles/quick-reference/clojure" Enter
+
+  tmux select-pane -t $sess:clojure.1
+
+  #----- git
+  tmux new-window -t $sess -n git
+  tmux send-keys -t $sess:git "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t $sess:git "cd ~/projects/dotfiles/quick-reference/git" Enter
+
+  tmux split-window -v -l 24 -t $sess:git
+  tmux send-keys -t $sess:git.2 "cd ~/projects/dotfiles/quick-reference/git" Enter
+
+  tmux split-window -h -l 50 -t $sess:git
+  tmux send-keys -t $sess:git.3 "cd ~/projects/dotfiles/quick-reference/git" Enter
+
+  tmux select-pane -t $sess:git.1
+
+  #-----
+
+  #select first
+  tmux select-window -t $sess:dotfiles
+
+  tmux -2 attach-session -t $sess
+
+}
+
