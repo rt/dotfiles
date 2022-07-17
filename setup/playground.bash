e0aef1e5a0a642d66e60d22cf47944cf821d6f93
rtsunoda <ryantsunoda@gmail.com>
2022-07-17 10:33:36 -1000

    add playground hooks

--- setup/playground.bash
+++ setup/playground.bash
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 alias setup.playground="setup_playground"
+alias p.pane="p_pane"
+alias p.plug="p_plug"
+
+p_plug() {
+  tmux new-window -t "$1" -n playground
+  tmux send-keys -t "$1":playground "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
+  tmux send-keys -t "$1":playground "cd ~/projects/playground" Enter
+  tmux send-keys -t "$1":playground Escape "c"
+}
+
+p_pane() {
+  tmux split-window -v -l 24 -t "$1":"$2" 
+  # because specifying pane 2 this needs to be executed in empty terminal
+  tmux send-keys -t "$1":"$2".2 "cd ~/projects/playground" Enter
+  tmux send-keys -t "$1":"$2".2 "vim.find" Enter
+  tmux swap-pane -U
+}
 
 setup_playground() {
 
@@ -10,14 +27,7 @@ setup_playground() {
   tmux new-session -s $sess -d -n top
   tmux send-keys -t $sess:top "printf '\033]2;%s\033\\' '$1'; '$@';" Enter
   tmux send-keys -t $sess:top "cd ~/projects/playground" Enter
-  tmux send-keys -t $sess:top "vim" Enter
-
-  tmux split-window -v -l 24 -t $sess:top
-  tmux send-keys -t $sess:top.2 "cd ~/projects/playground/setup" Enter
-  tmux send-keys -t $sess:top.2 "source ./index.bash" Enter
-  tmux send-keys -t $sess:top.2 "nvm use stable" Enter
-
-  tmux select-pane -t $sess:top.1
+  tmux send-keys -t $sess:top "nvm use stable" Enter
 
   plug.project.dotfiles
 
